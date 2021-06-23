ba64748713e92e46a4b18e9ad92702a8c6b8b532
pc <pc@MKP-L-028>
2021-06-23 09:46:42 +0200

    Add project files.

---
+++ QuizApp/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace QuizApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "1";
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(391, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "2";
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(51, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 61);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "3";
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(391, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 61);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "4";
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(12, 242);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(530, 23);
+            this.lblQuestion.TabIndex = 4;
+            this.lblQuestion.Text = "Question";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QuizApp.Properties.Resources.VSC1;
+            this.pictureBox1.Location = new System.Drawing.Point(91, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 214);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 496);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "QuizMath Game";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ QuizApp/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizApp
+{
+    public partial class Form1 : Form
+    {
+
+        // quiz game variables
+
+        int correctAnswer;
+        int questionNumber = 1;
+        int score;
+        int percentage;
+        int totalQuestions;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            askQuestion(questionNumber);
+
+            totalQuestions = 5;
+        }
+
+        private void checkAnswerEvent(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                score++;
+            }
+
+            if (questionNumber == totalQuestions)
+            {
+                // work out the percentage
+
+                percentage = (int)Math.Round((double)(score * 100) / totalQuestions);
+
+                MessageBox.Show(
+                    "Quiz Ended!" + Environment.NewLine +
+                    "You have answered " + score + " questions correctly." + Environment.NewLine +
+                    "Your total percentage is " + percentage + "%" + Environment.NewLine +
+                    "Click OK to play again"
+                    );
+
+                score = 0;
+                questionNumber = 0;
+                askQuestion(questionNumber);
+
+            }
+
+            questionNumber++;
+            askQuestion(questionNumber);
+
+        }
+
+        private void askQuestion(int qnum)
+        {
+
+            switch (qnum)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources.VSC;
+
+                    lblQuestion.Text = "Which visual control is linked to Microsoft on the list?";
+
+                    button1.Text = "LaunchPad";
+                    button2.Text = "BitBucket";
+                    button3.Text = "Azure DevOps";
+                    button4.Text = "GitHub";
+
+                    correctAnswer = 3;
+
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources.OS;
+
+                    lblQuestion.Text = "Which operation System is compatable with most Smartphones?";
+
+                    button1.Text = "Linux";
+                    button2.Text = "Windows";
+                    button3.Text = "Android";
+                    button4.Text = "MAcOs";
+
+                    correctAnswer = 3;
+
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources.CD;
+
+                    lblQuestion.Text = "Which computer divice most proggramers reccomend when coding?";
+
+                    button1.Text = "Smartphone";
+                    button2.Text = "Tablet";
+                    button3.Text = "Desktop";
+                    button4.Text = "Laptop";
+
+                    correctAnswer = 4;
+
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources.IDE;
+
+                    lblQuestion.Text = "Which IDE is mostly used for android applications?";
+
+                    button1.Text = "Android Studio";
+                    button2.Text = "Visual Studio";
+                    button3.Text = "Netbeans";
+                    button4.Text = "Eclipse";
+
+                    correctAnswer = 1;
+
+                    break;
+
+                case 5:
+                    pictureBox1.Image = Properties.Resources.PL;
+
+                    lblQuestion.Text = "what is the best language for sofrtware development?";
+
+                    button1.Text = "HTML 5";
+                    button2.Text = "Phython";
+                    button3.Text = "C++";
+                    button4.Text = "C#";
+
+                    correctAnswer = 3;
+
+                    break;
+
+            }
+
+
+
+        }
+
+    }
+}
